d1b0e3982fde1768d51d1d687f3d27bad6a379b2
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-06-25 21:26:49 +0100

    setup and initialize firebase

---
+++ src/helpers/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAdYB18S8x3yhhLa7Gu_p9VvZNmSNlL6zs",
+    authDomain: "jiraan-95d20.firebaseapp.com",
+    databaseURL: "https://jiraan-95d20.firebaseio.com",
+    projectId: "jiraan-95d20",
+    storageBucket: "jiraan-95d20.appspot.com",
+    messagingSenderId: "913166699986"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
