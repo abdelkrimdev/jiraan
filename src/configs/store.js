aa2f4e7f84d57e095812467f5a998b4a32d0c3e0
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-07-21 12:47:00 +0100

    add authentication reducers to the store

--- src/configs/store.js
+++ src/configs/store.js
@@ -2,9 +2,15 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import loggerMiddleware from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
+import { createUser } from '../reducers/create-user.reducer';
+import { loginUser } from '../reducers/login-user.reducer';
+import { currentUser } from '../reducers/current-user.reducer';
+
 const store = createStore(
     combineReducers({
-        
+        createUser,
+        loginUser,
+        currentUser
     }),
     applyMiddleware(
         loggerMiddleware,
