dbc3b261bc78e36135cda618b3f4b25803ccfaa2
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-07-21 10:19:02 +0100

    implement the current user function

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -1,11 +1,15 @@
 import firebase from '../configs/firebase';
 
 export const signUp = (email, password) => {
-    return await firebase.auth().createUserWithEmailAndPassword(email, password);
+    return firebase.auth().createUserWithEmailAndPassword(email, password);
 };
 
 export const signIn = (email, password) => {
-    return await firebase.auth().signInWithEmailAndPassword(email, password);
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+};
+
+export const currentUser = () => {
+    return firebase.auth().currentUser;
 };
 
 export const signOut = () => {
