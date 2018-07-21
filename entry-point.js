4f0084b9c200ba7567bd8e1441992e049a9e72d5
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-07-21 15:13:14 +0100

    change the entry point

---
+++ entry-point.js
@@ -0,0 +1,8 @@
+import { KeepAwake, registerRootComponent } from 'expo';
+import App from './src/App';
+
+if (process.env.NODE_ENV === 'development') {
+    KeepAwake.activate();
+}
+
+registerRootComponent(App);
