04b7ab2b128716c44e97cbd694a0d1ab373afe62
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-07-28 18:20:39 +0100

    configuring the header bar for the authentication stack

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,6 +1,7 @@
 import { createStackNavigator, createSwitchNavigator } from 'react-navigation'
 
 import { navigationConstants } from '../constants/navigation.constants'
+import { themeConstants } from '../constants/theme.constants'
 
 import SplashScreen from '../screens/splash.screen'
 import LoginScreen from '../screens/login.screen'
@@ -8,7 +9,18 @@ import Home from '../screens/Home'
 
 const authenticationStack = createStackNavigator(
   { [navigationConstants.LOGIN]: LoginScreen },
-  { initialRouteName: navigationConstants.LOGIN }
+  {
+    initialRouteName: navigationConstants.LOGIN,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: themeConstants.PRIMARY_COLOR
+      },
+      headerTintColor: themeConstants.LIGHT_COLOR,
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }
+  }
 )
 
 const applicationStack = createStackNavigator(
