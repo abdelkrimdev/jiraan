63791481c2b088a482741598ca8d717c1d0a55b9
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-07-28 18:18:14 +0100

    define theme colors

---
+++ src/constants/theme.constants.js
@@ -0,0 +1,11 @@
+export const themeConstants = {
+  // Theme Colors
+  PRIMARY_COLOR: '#8e44ad',
+  SECONDARY_COLOR: '#95a5a6',
+  SUCCESS_COLOR: '#27ae60',
+  DANGER_COLOR: '#e74c3c',
+  WARNING_COLOR: '#f1c40f',
+  INFO_COLOR: '#3498db',
+  LIGHT_COLOR: '#ecf0f1',
+  DARK_COLOR: '#2c3e50'
+}
