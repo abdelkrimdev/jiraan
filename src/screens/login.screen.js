03cb03bc5e6cc4bd95487f48ee2aa7c550aa39f2
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-07-29 14:29:16 +0100

    connect the login screen to the redux store

--- src/screens/login.screen.js
+++ src/screens/login.screen.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { Button, StyleSheet, View } from 'react-native'
 import { connect } from 'react-redux'
 
+import { authenticationActions } from '../actions/authentication.actions'
 import InputField from '../components/input-field.component'
+import { navigationConstants } from '../constants/navigation.constants'
 import { themeConstants } from '../constants/theme.constants'
 
 export class Login extends Component {
@@ -17,12 +19,16 @@ export class Login extends Component {
 
   handleSignIn = () => {
     const { email, password } = this.state
-    console.log(`Credentials: email -> ${email} password -> ${password}`)
+    if (email && password) {
+      this.props.signIn(email, password)
+    }
   }
 
   render () {
+    const { user } = this.props
     return (
       <View style={styles.container}>
+        {user && this.props.navigation.navigate(navigationConstants.APP)}
         <InputField
           keyboardType='email-address'
           placeholder='my-email@somewhere.com'
@@ -43,12 +49,15 @@ export class Login extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => {
+  const { fetching, user, error } = state.loginUser
+  return { fetching, user, error }
+}
 
-})
-
-const mapDispatchToProps = {
-
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (email, password) => dispatch(authenticationActions.signIn(email, password))
+  }
 }
 
 const styles = StyleSheet.create({
