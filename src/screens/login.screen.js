3fad3c55c9600057ac50afb9304331d948bab97e
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-07-29 11:49:02 +0100

    a basic implementation for the login screen

--- src/screens/login.screen.js
+++ src/screens/login.screen.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, View } from 'react-native'
 import { connect } from 'react-redux'
 
+import InputField from '../components/input-field.component'
+import { themeConstants } from '../constants/theme.constants'
+
 export class Login extends Component {
+  static navigationOptions = {
+    title: 'Login'
+  }
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleSignIn = () => {
+    const { email, password } = this.state
+    console.log(`Credentials: email -> ${email} password -> ${password}`)
+  }
+
   render () {
     return (
       <View style={styles.container}>
-        <Text>Login Screen</Text>
+        <InputField
+          keyboardType='email-address'
+          placeholder='my-email@somewhere.com'
+          onChangeText={(email) => this.setState({email})}
+        />
+        <InputField
+          secureTextEntry
+          placeholder='my-secret-password'
+          onChangeText={(password) => this.setState({password})}
+        />
+        <Button
+          title='Sign In'
+          onPress={this.handleSignIn}
+          color={themeConstants.PRIMARY_COLOR}
+        />
       </View>
     )
   }
@@ -23,9 +54,9 @@ const mapDispatchToProps = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    margin: 10,
+    padding: 20,
+    alignItems: 'center'
   }
 })
 
